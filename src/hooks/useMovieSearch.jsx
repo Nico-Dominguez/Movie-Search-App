a111b6eba77abb8999a06d930323ec2379c42a79
NicoADominguez <98nicodominguez@gmail.com>
2024-08-30 09:30:42 -0400

    hide api key

--- src/hooks/useMovieSearch.jsx
+++ src/hooks/useMovieSearch.jsx
@@ -5,7 +5,10 @@ export default function useMovieSearch() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [totalResults, setTotalResults] = useState(0);
-  const BASE_URL = `https://www.omdbapi.com/?apikey=c47d8db3&`;
+
+  const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+  console.log(apiKey); // Check if this outputs the correct API key
+  const BASE_URL = `https://www.omdbapi.com/?apikey=${apiKey}&`;
 
   const searchMovies = useCallback(async (searchTerm, page = 1) => {
     setIsLoading(true);
@@ -19,10 +22,17 @@ export default function useMovieSearch() {
     }
 
     try {
-      const response = await fetch(`${BASE_URL}s=${searchTerm}&page=${page}`);
+      const url = `${BASE_URL}s=${searchTerm}&page=${page}`;
+      console.log(url); // Log the complete URL
+      const response = await fetch(url);
       const data = await response.json();
-      setMovies(data.Search || []);
-      setTotalResults(parseInt(data.totalResults) || 0);
+
+      if (response.ok) {
+        setMovies(data.Search || []);
+        setTotalResults(parseInt(data.totalResults) || 0);
+      } else {
+        setError("Invalid API key or other authorization error");
+      }
     } catch (error) {
       setError("Error fetching movie data");
       console.error("Error fetching movie data:", error);
